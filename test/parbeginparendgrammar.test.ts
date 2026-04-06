c55a1432f061f1a3203285ec2cf2bf27433667f5
Lucas Marcos <640383+lucasmarcos@users.noreply.github.com>
2026-04-06 17:22:52 -0300

    fix: grammar tests

---
+++ test/parbeginparendgrammar.test.ts
@@ -0,0 +1,17 @@
+import { fileTests } from "@lezer/generator/dist/test";
+import { parser as forkJoinParser } from "../out/parBeginParEndParser.js";
+import fs from "node:fs";
+import path from "node:path";
+import { test } from "node:test";
+
+const corpusDir = "test/grammar/parbeginparend/corpus";
+
+for (const file of fs.readdirSync(corpusDir)) {
+  if (!file.endsWith(".txt")) continue;
+
+  const content = fs.readFileSync(path.join(corpusDir, file), "utf8");
+
+  for (const {name, run} of fileTests(content, file)) {
+    test(name, () => run(parbeginParendParser));
+  }
+}
