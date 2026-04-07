b19b6991ecdfce1ecb8bc722472cb323389618eb
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2026-04-07 15:42:39 -0300

    tests: more

--- test/bork.test.js
+++ test/bork.test.js
@@ -4,62 +4,90 @@ import { parser } from "../out/forkjoin/parser.js";
 import { resolve } from "../out/forkjoin/resolve.js";
 import { treewalk } from "../out/forkjoin/treewalk.js";
 
-const code = `
-VAR_H = 6;
-VAR_D = 3;
-
-FORK ROT_A;
-B;
-JOIN VAR_D, ROT_D, QUIT;
-
-ROT_A:
-  A;
-  FORK ROT_C;
-  JOIN VAR_D, ROT_D, QUIT;
-
-ROT_D:
-  D;
-  FORK ROT_E;
-  FORK ROT_G;
-  F;
-  JOIN VAR_H, ROT_H, QUIT;
-
-ROT_C:
-  C;
-  JOIN VAR_D, ROT_D, QUIT;
-
-ROT_E:
-  E;
-  JOIN VAR_H, ROT_H, QUIT;
-
-ROT_G:
-  G;
-  JOIN VAR_H, ROT_H, QUIT;
-
-ROT_H:
-  H;
-`;
-
 test("borked", () => {
+  const code = `
+    VAR_H = 6;
+    VAR_D = 3;
+
+    FORK ROT_A;
+    B;
+    JOIN VAR_D, ROT_D, QUIT;
+
+    ROT_A:
+      A;
+      FORK ROT_C;
+      JOIN VAR_D, ROT_D, QUIT;
+
+    ROT_D:
+      D;
+      FORK ROT_E;
+      FORK ROT_G;
+      F;
+      JOIN VAR_H, ROT_H, QUIT;
+
+    ROT_C:
+      C;
+      JOIN VAR_D, ROT_D, QUIT;
+
+    ROT_E:
+      E;
+      JOIN VAR_H, ROT_H, QUIT;
+
+    ROT_G:
+      G;
+      JOIN VAR_H, ROT_H, QUIT;
+
+    ROT_H:
+      H;
+  `;
+
   const tree = parser.parse(code);
   const walked = treewalk(code, tree);
   const elements = resolve(walked.threads);
 
   const nodeD = elements.nodes.find((n) => n.data.label === "D");
-  const nodeE = elements.nodes.find((n) => n.data.label === "E");
-  const nodeG = elements.nodes.find((n) => n.data.label === "G");
 
   assert.ok(nodeD, "D exists");
-  assert.ok(nodeE, "E exists");
-  assert.ok(nodeG, "G exists");
+
+  const nodesE = elements.nodes.filter((n) => n.data.label === "E");
+  const nodesG = elements.nodes.filter((n) => n.data.label === "G");
+
+  assert.strictEqual(nodesE.length, 1, "Just one E");
+  assert.strictEqual(nodesG.length, 1, "Just one G");
+
+  const nodeE = elements.nodes.find((n) => n.data.label === "E");
+  const nodeG = elements.nodes.find((n) => n.data.label === "G");
+  const nodeF = elements.nodes.find((n) => n.data.label === "G");
+  const nodeH = elements.nodes.find((n) => n.data.label === "G");
 
   const edgeDE = elements.edges.find(
     (e) => e.data.source === nodeD.data.id && e.data.target === nodeE.data.id,
   );
+
   const edgeDG = elements.edges.find(
     (e) => e.data.source === nodeD.data.id && e.data.target === nodeG.data.id,
   );
 
+  const edgeDF = elements.edges.find(
+    (e) => e.data.source === nodeD.data.id && e.data.target === nodeF.data.id,
+  );
+
+  const edgeEH = elements.edges.find(
+    (e) => e.data.source === nodeF.data.id && e.data.target === nodeH.data.id,
+  );
+
+  const edgeFH = elements.edges.find(
+    (e) => e.data.source === nodeF.data.id && e.data.target === nodeH.data.id,
+  );
+
+  const edgeGH = elements.edges.find(
+    (e) => e.data.source === nodeG.data.id && e.data.target === nodeH.data.id,
+  );
+
   assert.ok(edgeDE, "should have edge d -> e");
   assert.ok(edgeDG, "should have edge d -> g");
+  assert.ok(edgeDF, "should have edge d -> f");
+  assert.ok(edgeEH, "should have edge e -> h");
+  assert.ok(edgeFH, "should have edge f -> h");
+  assert.ok(edgeGH, "should have edge g -> h");
 });
--- test/wrong_join_var.test.js
+++ test/wrong_join_var.test.js
@@ -1,78 +1,193 @@
-const _code1 = `
-var_F = 2;
-var_G = 2;
-var_H = 2;
+import assert from "node:assert";
+import { test } from "node:test";
+import { parser } from "../out/forkjoin/parser.js";
+import { resolve } from "../out/forkjoin/resolve.js";
+import { treewalk } from "../out/forkjoin/treewalk.js";
 
-A;
-FORK ROT_C;
-FORK ROT_D;
-FORK ROT_E;
-B;
-JOIN var_F, ROT_F, QUIT;
+test("default rodolfo example 1", () => {
+  const ex1 = `
+    var_F = 2;
+    var_G = 2;
+    var_H = 2;
 
-ROT_C: C;
-       JOIN var_F, ROT_F, QUIT;
+    A;
+    FORK ROT_C;
+    FORK ROT_D;
+    FORK ROT_E;
+    B;
+    JOIN var_F, ROT_F, QUIT;
 
-ROT_D: D;
-       JOIN var_G, ROT_G, QUIT;
+    ROT_C: C;
+          JOIN var_F, ROT_F, QUIT;
 
-ROT_E: E;
-       JOIN var_H, ROT_H, QUIT;
+    ROT_D: D;
+          JOIN var_G, ROT_G, QUIT;
 
-ROT_F: F;
-       JOIN var_G, ROT_G, QUIT;
+    ROT_E: E;
+          JOIN var_H, ROT_H, QUIT;
 
-ROT_G: G;
-       JOIN var_H, ROT_H, QUIT;
+    ROT_F: F;
+          JOIN var_G, ROT_G, QUIT;
 
-ROT_H: H;
-       QUIT;
-`;
+    ROT_G: G;
+          JOIN var_H, ROT_H, QUIT;
 
-const _code2 = `
-var_E = 2;
+    ROT_H: H;
+          QUIT;
+  `;
 
-FORK ROT_C;
-A;
-B;
-JOIN var_E, ROT_E, QUIT;
+  const tree = parser.parse(ex1);
+  const { threads, errors } = treewalk(ex1, tree);
+  const resolved = resolve(threads);
 
-ROT_C: C;
-       D;
-       JOIN var_E, ROT_E, QUIT;
+  assert.strictEqual(errors.length, 0, "No errors in ex1");
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "A"),
+    "A exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "B"),
+    "B exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "C"),
+    "C exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "D"),
+    "D exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "E"),
+    "E exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "F"),
+    "F exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "G"),
+    "G exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "H"),
+    "H exists",
+  );
+});
 
-ROT_E: E;
-       F;
-       QUIT;
+test("default rodolfo example 2", () => {
+  const ex2 = `
+    var_E = 2;
 
-`;
+    FORK ROT_C;
+    A;
+    B;
+    JOIN var_E, ROT_E, QUIT;
 
-const _code3 = `
-var_R5 = 2;
-var_R6 = 2;
-var_R7 = 2;
+    ROT_C: C;
+          D;
+          JOIN var_E, ROT_E, QUIT;
 
-FORK ROT_R4;
-R1;
-FORK ROT_R3;
-R2;
-JOIN var_R5, ROT_R5, QUIT;
+    ROT_E: E;
+          F;
+          QUIT;
+  `;
 
-ROT_R4: R4;
-        JOIN var_R6, ROT_R6, QUIT;
+  const tree = parser.parse(ex2);
+  const { threads, errors } = treewalk(ex2, tree);
+  const resolved = resolve(threads);
 
-ROT_R3: R3;
-        FORK ROT_R6';
-        JOIN var_R5, ROT_R5, QUIT;
+  assert.strictEqual(errors.length, 0, "No errors in ex2");
 
-ROT_R5: R5;
-	      JOIN var_R7, ROT_R7, QUIT;
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "A"),
+    "A exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "B"),
+    "B exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "C"),
+    "C exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "D"),
+    "D exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "E"),
+    "E exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "F"),
+    "F exists",
+  );
+});
 
-ROT_R6: R6;
-	      JOIN var_R7, ROT_R7, QUIT;
+test("default rodolfo example 3", () => {
+  const ex3 = `
+    var_R5 = 2;
+    var_R6 = 2;
+    var_R7 = 2;
 
-ROT_R6': JOIN var_R6, ROT_R6, QUIT;
+    FORK ROT_R4;
+    R1;
+    FORK ROT_R3;
+    R2;
+    JOIN var_R5, ROT_R5, QUIT;
 
-ROT_R7: R7;
-	      QUIT;
-`;
+    ROT_R4: R4;
+            JOIN var_R6, ROT_R6, QUIT;
+
+    ROT_R3: R3;
+            FORK ROT_R6';
+            JOIN var_R5, ROT_R5, QUIT;
+
+    ROT_R5: R5;
+    	      JOIN var_R7, ROT_R7, QUIT;
+
+    ROT_R6: R6;
+    	      JOIN var_R7, ROT_R7, QUIT;
+
+    ROT_R6': JOIN var_R6, ROT_R6, QUIT;
+
+    ROT_R7: R7;
+    	      QUIT;
+  `;
+
+  const tree = parser.parse(ex3);
+  const { threads, errors } = treewalk(ex3, tree);
+  const resolved = resolve(threads);
+
+  assert.strictEqual(errors.length, 0, "No errors in ex3");
+
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "R1"),
+    "R1 exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "R2"),
+    "R2 exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "R3"),
+    "R3 exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "R4"),
+    "R4 exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "R5"),
+    "R5 exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "R6"),
+    "R6 exists",
+  );
+  assert.ok(
+    resolved.nodes.find((n) => n.data.label === "R7"),
+    "R7 exists",
+  );
+});
