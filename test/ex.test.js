dc1c981cf3fc17c7cf248adaf89e7c4b9e4662f8
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2026-04-07 15:32:54 -0300

    tests: add ex.test

--- test/ex.test.js
+++ test/ex.test.js
@@ -1,12 +1,41 @@
-const _code = `
-A;
-FORK ROT_C;
-JOIN VAR_C, ROT_D, QUIT;
+import assert from "node:assert";
+import { test } from "node:test";
+import { parser } from "../out/forkjoin/parser.js";
+import { treewalk } from "../out/forkjoin/treewalk.js";
 
-ROT_C:
-  C;
-  JOIN VAR_C, ROT_D, QUIT;
+test("ex shouldReportTwoMissingJoinVariableErrorsForVAR_C", () => {
+  const code = `
+    A;
+    FORK ROT_C;
+    B;
+    JOIN VAR_C, ROT_D, QUIT;
 
-ROT_D:
-  D;
-`;
+    ROT_C:
+      C;
+      JOIN VAR_C, ROT_D, QUIT;
+
+    ROT_D:
+      D;
+  `;
+
+  const tree = parser.parse(code);
+  const { errors } = treewalk(code, tree);
+
+  assert.strictEqual(errors.length, 2, "Two errors");
+
+  assert.strictEqual(
+    errors[0].type,
+    "join-missing-variable",
+    "First error is undefined label",
+  );
+
+  assert.strictEqual(errors[0].label, "VAR_C", "First error on VAR_C label");
+
+  assert.strictEqual(
+    errors[1].type,
+    "join-missing-variable",
+    "Second error is undefined label",
+  );
+
+  assert.strictEqual(errors[1].label, "VAR_C", "Second error on VAR_C label");
+});
